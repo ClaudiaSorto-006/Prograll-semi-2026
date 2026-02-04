a76d8719a750462ea7b07d9cdf81447a217f2ac2
CLAUDIA ARELY SORTO MELENDEZ <USSS002825@ugb.edu.sv>
2026-02-04 16:25:35 -0600

    Se agrego la funcianalidad de la calculadora basica

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.miprimeraapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+TextView tempVal;
 
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(v -> calcular());
+
     }
-}
+
+    private void calcular() {
+        tempVal = findViewById(R.id.txtNum1);
+        Double Num1 = Double.parseDouble(tempVal.getText().toString());
+
+        tempVal = findViewById(R.id.txtNum2);
+        Double Num2 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = Num1 + Num2;
+
+        tempVal = findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta:" + respuesta);
+    }
+};
