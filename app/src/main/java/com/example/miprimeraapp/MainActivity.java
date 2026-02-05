3b74da3712810a2ef5e907c31815b3ebbd34b89d
CLAUDIA ARELY SORTO MELENDEZ <USSS002825@ugb.edu.sv>
2026-02-05 10:28:27 -0600

    Se agrego la funcianalidad de la calculadora basica, suma, resta, multiplicacion y divicion

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.miprimeraapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 TextView tempVal;
 
 Button btn;
+RadioButton opt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +34,29 @@ Button btn;
         tempVal = findViewById(R.id.txtNum2);
         Double Num2 = Double.parseDouble(tempVal.getText().toString());
 
-        double respuesta = Num1 + Num2;
+        double respuesta = 0;
+
+       opt=findViewById(R.id.optSuma);
+       if(opt.isChecked()){
+           respuesta = Num1 + Num2;}
+
+       opt=findViewById(R.id.optResta);
+        if(opt.isChecked()){
+              respuesta = Num1 - Num2;}
+
+        opt=findViewById(R.id.optMultiplicar);
+        if(opt.isChecked()){
+             respuesta = Num1 * Num2;}
+
+        opt=findViewById(R.id.optDividir);
+        if(opt.isChecked()){
+             respuesta = Num1 / Num2;}
+
+        opt=findViewById(R.id.optPorcentaje);
+        if(opt.isChecked()){
+            respuesta = Num1*Num2/ 100;
+        ;}
+
 
         tempVal = findViewById(R.id.lblRespuesta);
         tempVal.setText("Respuesta:" + respuesta);
